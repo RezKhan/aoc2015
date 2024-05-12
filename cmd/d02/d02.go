137563bac8086865a5173fa2f65578f879747d47
Rez <probablyrez@gmail.com>
2024-05-12 11:23:40 +1000

    day 2, part 2

--- cmd/d02/d02.go
+++ cmd/d02/d02.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	rf "readfile"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -39,12 +40,44 @@ func partOne(lines []string) {
 		}
 		area += minside
 		totalArea += area
-		fmt.Println("box: ", line, "area: ", area, "total: ", totalArea)
+		// fmt.Println("box: ", line, "area: ", area, "total: ", totalArea)
 	}
+	fmt.Println("Total area: ", totalArea)
+}
+
+func partTwo(lines []string) {
+	// Calculating the ribbon requires calculating the perimeter of of the smallest side
+	// in a box of 2x3x4 the smallest face is 2x3, which means 2*2 + 2*3 for a tota of 10
+	// then the bow is 2x3x4 or 2*3*4 for a total of 24, and a final total ribbon length of 34
+	totalRibbon := 0
+	for _, line := range lines {
+		var nums []int
+		strnums := strings.Split(line, "x")
+		for _, snum := range strnums {
+			n, err := strconv.Atoi(snum)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			nums = append(nums, n)
+		}
+		// since we always have 3 numbers to represent the dimensions and the faces are interchangeable
+		// we can sort the array to get the two smallest values at the start
+		sort.Ints(nums)
+		wrap := (2 * nums[0]) + (2 * nums[1])
+		bow := 1
+		for _, nums := range nums {
+			bow *= nums
+		}
+		ribbon := wrap + bow
+		totalRibbon += ribbon
+	}
+	fmt.Println("Ribbon needed:", totalRibbon)
 }
 
 func main() {
 	// lines := rf.ReadFile("test.txt")
 	lines := rf.ReadFile("input.txt")
 	partOne(lines)
+	partTwo(lines)
 }
