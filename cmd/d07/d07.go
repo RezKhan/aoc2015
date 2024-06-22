a6807610ba72bc445fdd133ecc92a0a7d56d087c
Rez <probablyrez@gmail.com>
2024-06-22 11:00:54 +1000

    minor change

--- cmd/d07/d07.go
+++ cmd/d07/d07.go
@@ -3,20 +3,41 @@ package main
 import (
 	"fmt"
 	rf "readfile"
+	"strconv"
 	"strings"
 )
 
+func parseCommandsIntoSignals(cmd map[string]string, signals map[string]int) {
+	// fmt.Println("elements in map: ", len(cmd))
+	for k, str := range cmd {
+		// fmt.Println(k, str, len(strings.Split(str, " ")))
+		if len(strings.Split(str, " ")) == 1 {
+			n, err := strconv.Atoi(str)
+			if err != nil {
+				continue
+			}
+			signals[k] = n
+		}
+	}
+	fmt.Println(signals)
+}
+
 func partOne(lines []string) {
+	signals := make(map[string]int)
+	commands := make(map[string]string)
 	for _, line := range lines {
 		tline := strings.Split(line, "->")
-		tsource := strings.Split(strings.TrimRight(tline[0], " "), " ")
+		tsource := strings.TrimRight(tline[0], " ")
 		destination := strings.TrimSpace(tline[1])
-		fmt.Println(len(tsource), tsource, destination)
+		commands[destination] = tsource
+		// fmt.Println(len(tsource), tsource, destination)
 	}
+	// target := "e"
+	parseCommandsIntoSignals(commands, signals)
 }
 
 func main() {
-	fmt.Println(123 & 456)
+	// fmt.Println(123 & 456)
 	lines := rf.ReadFile("./test.txt")
 	partOne(lines)
 }
