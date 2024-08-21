c93df96ac50652deb70a9f1968b4188ac9c07e0e
Rez <probablyrez@gmail.com>
2024-08-21 20:48:14 +1000

    Fixed recursive case for test inputs

--- cmd/d07/d07.go
+++ cmd/d07/d07.go
@@ -2,63 +2,83 @@ package main
 
 import (
 	"fmt"
-	"log"
 	rf "readfile"
 	"strconv"
 	"strings"
 )
 
-func parseCommandsIntoSignals(cmd map[string]string, target string, signals map[string]int) map[string]int {
-	fmt.Println(target, cmd[target])
+func parseCommandsIntoSignals(cmd map[string]string, target string, signals map[string]uint16) map[string]uint16 {
+	fmt.Println("Entering: ", target, cmd[target])
 	strcmd := strings.Split(cmd[target], " ")
+	// fmt.Println(len(strcmd), strcmd[0])
 	// BASE CASE
 	if len(strcmd) == 1 {
 		n, err := strconv.Atoi(strcmd[0])
 		if err != nil {
+			fmt.Println("In base case received error:", err)
 			return nil
 		}
-		signals[target] = n
+		signals[target] = uint16(n)
 		return signals
 	}
-	// SHIFT OPERATION:
-	if len(strcmd) == 3 && strings.Contains(strcmd[1], "SHIFT") {
-		// Handle shift operation
-		// Check if the value to be shifted is already populated
-		if v, ok := signals[strcmd[0]]; ok {
-			log.Println("v, ok:", v, ok)
-			shiftval, err := strconv.Atoi(strcmd[2])
-			log.Println(shiftval)
-			if err != nil {
-				log.Fatalln(err, "Shift value could not be determined")
-			}
-			if strcmd[1] == "LSHIFT" {
-				fmt.Println("Shifting", signals[strcmd[0]], "left")
-				signals[target] = signals[strcmd[0]] >> shiftval
-			}
-			if strcmd[1] == "RSHIFT" {
-				fmt.Println("Shifting", signals[strcmd[0]], "right")
-				signals[target] = signals[strcmd[0]] << shiftval
-			}
-			fmt.Println(v)
-		} else {
-			// RECURSIVE CASE
-			fmt.Println("v not found")
+	// RECURSIVE CASES
+	// Check if key exists in signals if not, go recursive
+	if strcmd[0] != "NOT" {
+		_, ok := signals[target]
+		if !ok {
 			signals = parseCommandsIntoSignals(cmd, strcmd[0], signals)
 		}
+	}
+
+	if strcmd[0] == "NOT" {
+		_, ok := signals[strcmd[1]]
+		if !ok {
+			signals = parseCommandsIntoSignals(cmd, strcmd[1], signals)
+		}
+	}
+
+	if strings.Contains(strcmd[1], "AND") || strings.Contains(strcmd[1], "OR") {
+		_, ok := signals[strcmd[2]]
+		if !ok {
+			signals = parseCommandsIntoSignals(cmd, strcmd[2], signals)
+		}
+	}
+
+	num := 0
+	if len(strcmd) == 3 && strings.Contains(strcmd[1], "SHIFT") {
+		n, err := strconv.Atoi(strcmd[2])
+		if err != nil {
+			fmt.Println("Unable to parse into string", err)
+			return nil
+		}
+		num = n
+	}
+
+	if strcmd[1] == "LSHIFT" {
+		signals[target] = signals[strcmd[0]] << num
 		return signals
 	}
-	// AND | OR OPERATION
-	if len(strcmd) == 3 && !strings.Contains(strcmd[1], "SHIFT") {
+	if strcmd[1] == "RSHIFT" {
+		signals[target] = signals[strcmd[0]] >> num
+		return signals
 	}
-	// NOT OPERATION
-	if len(strcmd) == 2 {
-
+	if strings.Contains(strcmd[1], "AND") {
+		signals[target] = signals[strcmd[0]] & signals[strcmd[2]]
+		return signals
+	}
+	if strings.Contains(strcmd[1], "OR") {
+		signals[target] = signals[strcmd[0]] | signals[strcmd[2]]
+	}
+	if strings.Contains(strcmd[0], "NOT") {
+		x := uint16(signals[strcmd[1]])
+		signals[target] = ^x
+		return signals
 	}
 	return signals
 }
 
 func partOne(lines []string) {
-	signals := make(map[string]int)
+	signals := make(map[string]uint16)
 	commands := make(map[string]string)
 	for _, line := range lines {
 		tline := strings.Split(line, "->")
@@ -66,13 +86,17 @@ func partOne(lines []string) {
 		destination := strings.TrimSpace(tline[1])
 		commands[destination] = tsource
 	}
-	target := "f"
+	target := "h"
+	// for k, v := range commands {
+	// 	fmt.Println("command:", k, " - has value: ", v)
+	// }
 	signals = parseCommandsIntoSignals(commands, target, signals)
-	fmt.Println(signals)
+	fmt.Println("Target value for:", target, " = ", signals[target])
 }
 
 func main() {
 	// fmt.Println(123 & 456)
 	lines := rf.ReadFile("./test.txt")
+	// lines := rf.ReadFile("./input.txt")
 	partOne(lines)
 }
