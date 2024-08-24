16c66bad8ddc83d1806fc2655e50ba7f72a4117f
Rez <probablyrez@gmail.com>
2024-08-24 20:35:02 +1000

    Failing some cases, but further along

--- cmd/d07/d07.go
+++ cmd/d07/d07.go
@@ -8,22 +8,25 @@ import (
 )
 
 func parseCommandsIntoSignals(cmd map[string]string, target string, signals map[string]uint16) map[string]uint16 {
-	fmt.Println("Entering: ", target, cmd[target])
+	fmt.Println("Entering target: ", target, "=", cmd[target])
+	if target == "" {
+		fmt.Println("You broke it")
+		return nil
+	}
 	strcmd := strings.Split(cmd[target], " ")
 	// fmt.Println(len(strcmd), strcmd[0])
+
+	cmd0, cerr := strconv.Atoi(strcmd[0])
 	// BASE CASE
-	if len(strcmd) == 1 {
-		n, err := strconv.Atoi(strcmd[0])
-		if err != nil {
-			fmt.Println("In base case received error:", err)
-			return nil
-		}
-		signals[target] = uint16(n)
+	if len(strcmd) == 1 && cerr == nil {
+		signals[target] = uint16(cmd0)
 		return signals
 	}
 	// RECURSIVE CASES
+	// The LEFT or RIGHT side may contain a number, and this needs to be checked for
 	// Check if key exists in signals if not, go recursive
-	if strcmd[0] != "NOT" {
+	if strcmd[0] != "NOT" && cerr != nil { // adding this nil check lets it get passed a couple of cases
+		// needs more work
 		_, ok := signals[target]
 		if !ok {
 			signals = parseCommandsIntoSignals(cmd, strcmd[0], signals)
@@ -37,23 +40,23 @@ func parseCommandsIntoSignals(cmd map[string]string, target string, signals map[
 		}
 	}
 
-	if strings.Contains(strcmd[1], "AND") || strings.Contains(strcmd[1], "OR") {
+	if strcmd[1] == "AND" || strcmd[1] == "OR" {
 		_, ok := signals[strcmd[2]]
 		if !ok {
 			signals = parseCommandsIntoSignals(cmd, strcmd[2], signals)
 		}
 	}
 
+	// ASSIGN SIGNALS TO WIRES
 	num := 0
-	if len(strcmd) == 3 && strings.Contains(strcmd[1], "SHIFT") {
+	if len(strcmd) == 3 && strcmd[1] == "SHIFT" {
 		n, err := strconv.Atoi(strcmd[2])
 		if err != nil {
-			fmt.Println("Unable to parse into string", err)
+			fmt.Println(err)
 			return nil
 		}
 		num = n
 	}
-
 	if strcmd[1] == "LSHIFT" {
 		signals[target] = signals[strcmd[0]] << num
 		return signals
@@ -62,14 +65,14 @@ func parseCommandsIntoSignals(cmd map[string]string, target string, signals map[
 		signals[target] = signals[strcmd[0]] >> num
 		return signals
 	}
-	if strings.Contains(strcmd[1], "AND") {
+	if strcmd[1] == "AND" {
 		signals[target] = signals[strcmd[0]] & signals[strcmd[2]]
 		return signals
 	}
-	if strings.Contains(strcmd[1], "OR") {
+	if strcmd[1] == "OR" {
 		signals[target] = signals[strcmd[0]] | signals[strcmd[2]]
 	}
-	if strings.Contains(strcmd[0], "NOT") {
+	if strcmd[0] == "NOT" {
 		x := uint16(signals[strcmd[1]])
 		signals[target] = ^x
 		return signals
@@ -86,7 +89,7 @@ func partOne(lines []string) {
 		destination := strings.TrimSpace(tline[1])
 		commands[destination] = tsource
 	}
-	target := "h"
+	target := "a"
 	// for k, v := range commands {
 	// 	fmt.Println("command:", k, " - has value: ", v)
 	// }
@@ -96,7 +99,7 @@ func partOne(lines []string) {
 
 func main() {
 	// fmt.Println(123 & 456)
-	lines := rf.ReadFile("./test.txt")
-	// lines := rf.ReadFile("./input.txt")
+	// lines := rf.ReadFile("./test.txt")
+	lines := rf.ReadFile("./input.txt")
 	partOne(lines)
 }
